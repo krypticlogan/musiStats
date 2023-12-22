72317e90f35679b837dbd97914354c102adb412d
Logan Jones <logan.zchjones@gmail.com>
2023-12-21 20:09:13 -0600

    access granted

--- flask_app.py
+++ flask_app.py
@@ -4,24 +4,16 @@ from flask import Flask, request, url_for, session, redirect, render_template
 import os
 from dotenv import load_dotenv
 import random as ran
+import json
 
+code = None
+USERNAME = ''
 
 def load(path=None, file=None):
     load_dotenv(path, file)
 
 load()
 
-#apis
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-spotify_scope = "user-read-recently-played"+"%20"+"user-top-read"+"%20"+"user-library-read"+"%20"+"user-read-currently-playing"
-#redirect_uri='http://127.0.0.1:5000/stats/'
-redirect_uri='https://www.musistats.net/stats'
-##spotify auth
-def spotify_auth():
-    #redirect_uri=url_for('spotifyUserLogin',_external=True)
-    sp = SpotifyOAuth(client_id =SPOTIFY_CLIENT_ID, client_secret = SPOTIFY_CLIENT_SECRET, redirect_uri=redirect_uri, scope = spotify_scope)
-    return sp
 
 def generate_key():
     key = ''
@@ -29,6 +21,21 @@ def generate_key():
         n = ran.randint(0,100)
         key = key + str(n)
     return key
+
+#apis
+SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+SPOTIPY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+spotify_scope = "user-read-recently-played"+"%20"+"user-top-read"+"%20"+"user-library-read"+"%20"+"user-read-currently-playing"
+# redirect_uri='http://127.0.0.1:5000/stats'
+state = generate_key()
+redirect_uri='https://www.musistats.net/stats'
+##spotify auth
+def spotify_auth():
+    #redirect_uri=url_for('spotifyUserLogin',_external=True)
+    sp = spotipy.oauth2.SpotifyOAuth(client_id =SPOTIPY_CLIENT_ID, client_secret = SPOTIPY_CLIENT_SECRET, redirect_uri=redirect_uri, scope = spotify_scope, state=state)
+    return sp
+
+
 
     ##apple auth
 def apple_auth():
@@ -57,11 +64,48 @@ def login():
 
 @app.route('/stats', methods=['GET'])
 def stats():
-    return render_template('tracks.html')
+    auth = SpotifyOAuth(client_id =SPOTIPY_CLIENT_ID,client_secret = SPOTIPY_CLIENT_SECRET , redirect_uri=redirect_uri, scope = spotify_scope, state=state)
+
+
+    access_token = auth.get_cached_token()
+    sp = spotipy.Spotify(auth_manager=auth)
+
+    if access_token: #checks for access token
+
+        result = sp.current_user() #gets the current user
+
+        #parsing json data
+        USERNAME = (result['display_name'])
+        
+    
+        return render_template('tracks.html', username=USERNAME)
+    
+    else:
+     code = request.args.get('code')
+     auth.get_access_token(code, as_dict=False)
+     
+
+     return redirect("/tracks")
+    
+    
 
 @app.route('/user')#for signed-in users
 def user():
-    return render_template('user.html')
+    auth = SpotifyOAuth(client_id =SPOTIPY_CLIENT_ID,client_secret = SPOTIPY_CLIENT_SECRET , redirect_uri=redirect_uri, scope = spotify_scope, state=state)
+    sp = spotipy.Spotify(auth_manager=auth)
+
+    access_token = auth.get_cached_token()
+    if access_token: 
+
+        result = sp.current_user()
+
+        USERNAME = (result['display_name'])
+        userImage = (result['images'][0]['url'])
+    
+        return render_template('user.html', username=USERNAME, userImage=userImage)
+    
+    else:
+        return render_template('user.html')
 
     #redirects and authentications
 @app.route('/sp', methods=['POST'])#for spotify sign-ups
@@ -81,4 +125,4 @@ def appleUserLogin():
     return render_template('oops.html')
 
 
-#app.run()
+# app.run()
